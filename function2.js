d394ceaac4b93137958fbd8149e6c7de8fd88636
ran002 <sabanarandy98@gmail.com>
2022-11-06 17:05:45 +0700

    tambah

---
+++ function2.js
@@ -0,0 +1,35 @@
+// let a = 8;
+// let b = 3; 
+// let volumeA;
+// let volumeB;
+// let total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+
+// total = volumeA + volumeB;
+
+// console.log(total);
+
+// function jumlahVolumeKubus(a,b) {
+//     let volumeA;
+//     let VolumeB;
+//     let total;
+
+//     volumeA = a * a * a;
+//     VolumeB = b * b * b;
+
+//     total = volumeA + VolumeB;
+
+//     return total;
+// }  
+
+// alert (jumlahVolumeKubus(8,3));
+// alert (jumlahVolumeKubus(10,15));
+
+function jumlahVolumeKubus(a,b) {
+    return a * a * a + b * b * b;
+}  
+
+alert (jumlahVolumeKubus(8,3));
+alert (jumlahVolumeKubus(10,15));
